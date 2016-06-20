c8107a512e6ef848f0be3575996235d51a36591b
niksmac <nikhil@lightrains.com>
2016-06-20 15:13:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lightrainstech.github.io
+
+[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)
---
+++ assets/js/site.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function($) {
+  jQuery('.dropdown').dropdown();
+  $('.logoo').popup();
+  $('.special.cards .image').dimmer({on: 'hover'});
+  $('.ui.embed').embed();
+
+  var pathname = window.location.pathname;
+  $("#menu > a.item", ".labels > a").each(function(index) {
+    if (pathname.toUpperCase().indexOf($(this).attr('href').toUpperCase()) != -1)
+      $(this).addClass("active");
+  });
+});
+
+jQuery(window).load(function() {
+  var colWidth = $('.work').width();
+  var $grid = $('.portfolio').isotope({
+    itemSelector: '.work',
+    masonry: {
+      columnWidth: colWidth,
+      fitWidth: true,
+      gutter: 35
+    }
+  });
+  $('#filters').on( 'click', 'a', function() {
+    $("#filters a").removeClass('black');
+    $( this ).addClass('black');
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+});
+
+if(jQuery().lightbox) {
+  lightbox.option({
+    'disableScrolling': true,
+  });
+}

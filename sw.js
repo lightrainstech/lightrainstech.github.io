cb1054654f3d055a259d3d07c95c928202fce49d
niksmac <hello@nikhilmohan.in>
2018-01-13 11:08:12 +0530

    Changed service worker strategy

--- sw.js
+++ sw.js
@@ -1287,8 +1287,8 @@ self.addEventListener('fetch', function(event) {
 
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
       })
     );
   });

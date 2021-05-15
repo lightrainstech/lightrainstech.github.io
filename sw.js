c49080db95593f1be1d446bcb26b6ec8f8a77da2
niksmac <nikhil@lightrains.com>
2021-05-15 10:23:57 +0530

    Add lazy load images

--- sw.js
+++ sw.js
@@ -1878,7 +1878,7 @@ var urlsToCache = [];
   
 
 
-var CACHE_NAME = 'lightrains-technolabs-cache-v241';
+var CACHE_NAME = 'lightrains-technolabs-cache-v242';
 
 self.addEventListener('install', function(event) {
   // Perform install steps
